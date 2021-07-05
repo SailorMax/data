d71e1e60bf3db6560dd7905f92d75de11a973a09
Maxim <sailormax@live.com>
2021-07-05 14:15:37 +0300

    covid-19 / widget / deaths data

--- covid-19/scripts/widget/lib/widget.js
+++ covid-19/scripts/widget/lib/widget.js
@@ -661,6 +661,15 @@ class Covid19Widget
 					.text( self.WORDS["unknown"] );
 			}
 
+			var deaths = last_day_data.deaths;
+			var recovered = last_day_data.recovered;
+			var deaths_per_recovered = (deaths ? 100 : 0);
+			if (recovered > 0)
+				deaths_per_recovered = Covid19DataTools.GetFormattedNumber(Math.round((deaths / recovered)*100000)/1000, true);
+			d3_box.select(".deaths_per_recovered TD:nth-child(2)")
+				.text( deaths_per_recovered + "%" )
+				.attr("title", Covid19DataTools.GetFormattedNumber(deaths, true) + " / " + Covid19DataTools.GetFormattedNumber(recovered, true));
+
 			box["myCalcDays"] = calc_days;	// remember for country switch
 			box["myScale"] = scale;
 		};
@@ -806,6 +815,14 @@ class Covid19Widget
 					.select(function(){ return this.parentNode; })
 				.select(function(){ return this.parentNode; })
 			.select(function(){ return this.parentNode; })
+			.append("td");
+
+		// deaths per recovered
+		d3_table.append("tr")
+			.attr("class", "deaths_per_recovered")
+			.append("td")
+				.text( this.WORDS["deaths_per_recovered"].charAt(0).toUpperCase() + this.WORDS["deaths_per_recovered"].slice(1) )
+				.select(function(){ return this.parentNode; })
 			.append("td");
 
 		// attach element to DOM
